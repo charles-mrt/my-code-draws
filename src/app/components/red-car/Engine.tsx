239fd24b544b134df62660f8ce334562bee03d33
charles-mrt <charlesmartins.ads@gmail.com>
2023-11-03 10:36:27 -0300

    feat: drawing red car

---
+++ src/app/components/red-car/Engine.tsx
@@ -0,0 +1,21 @@
+
+export const Engine = () => {
+  return (
+
+
+    <div>
+
+      <div className="w-12 h-2 bg-black m-auto mb-4 mt-[-12px] border-2 border-black" />
+
+      <div className="overflow-hidden">
+
+        <div className="flex mt-[-10px]">
+          <div className="w-8 h-4 rounded-full bg-gray-800 border-2 border-gray-900" />
+          <div className="-mt-1 w-10 h-6 rounded-full bg-gray-800 border-2 border-gray-900" />
+          <div className="w-8 h-4 rounded-full bg-gray-800 border-2 border-gray-900" />
+        </div>
+
+      </div>
+
+    </div>)
+}
---
+++ src/app/components/red-car/Light.tsx
@@ -0,0 +1,16 @@
+
+
+export const Light = () => {
+  return (
+    <div>
+      <div className="w-24 h-2  border border-black bg-gradient-to-b from-red-600 via-red-600 to-red-800">
+        <div className="bg-white w-1/6 h-full m-auto" />
+      </div>
+      
+      <div className="w-24 h-2 border border-black bg-gradient-to-b from-red-600 via-red-600 to-red-800">
+        <div className="bg-white w-1/6 h-full m-auto" />
+      </div>
+    </div>
+
+  )
+}
