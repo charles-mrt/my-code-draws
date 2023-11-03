5a664d1173e2d074d6de5413c17f1c309ff2005c
charles-mrt <charlesmartins.ads@gmail.com>
2023-11-03 18:51:23 -0300

    feat: implemented background image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import { VT323 } from 'next/font/google'
 import Image from 'next/image'
 
-import background from  '../../public/image/background.jpg'
+import background from '../../public/image/background.jpg'
 
 const vt323font = VT323({
   subsets: ['latin'],
@@ -29,9 +29,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans w-screen h-screen flex justify-center items-center bg-white ${vt323font.variable}`}>
-        {children}
-        {/* <Image src={background} alt="background" className="object-fill w-screen h-screen"/> */}
+      <body className={`relative font-sans antialiased w-screen h-screen flex justify-center items-center bg-white ${vt323font.variable}`}>
+
+        <div className="z-10 absolute">{children}</div>
+
+        <div className="w-screen h-screen relative z-0">
+          <div className="w-full h-screen  absolute z-10 bg-gradient-to-b from-red-600 via-purple-600 to-red-600 opacity-80 " />
+          <Image
+            src={background}
+            alt="background"
+            layout='fill'
+            objectFit='cover'
+            objectPosition='center'
+          />
+        </div>
+
       </body>
     </html>
   );
