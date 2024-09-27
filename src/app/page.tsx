cf21a1cc9fa0a7c9f19218e7eac766b05c2848ec
charles-mrt <charlesmartins.ads@gmail.com>
2024-09-27 16:29:06 -0300

    fix: add muted attribute to video element and import About component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { ChevronsRight } from "lucide-react";
 import Link from "next/link";
+import { About } from "./components/About";
 
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
       title: "top gear... go go",
       url: "pages/top-gear"
     },
-   
+
   ]
 
   return (
@@ -22,19 +23,20 @@ export default function Home() {
       <span className="text-6xl welcome-shadow" >bem vindo a </span>
       <h1 className="text-9xl  title-shadow" >My Code Draws </h1>
 
-      <video autoPlay loop className="fixed top-0 left-0 -z-10">
+      <video autoPlay loop muted className="fixed top-0 left-0 -z-10">
         <source src="/assets/video/background.mp4" type="video/mp4" />
       </video>
 
-      <div className="flex flex-col mt-6">
-       
+      <div className="mt-6 max-w-2xl flex flex-col justify-center">
+
         <ul>
 
           {pages.map((page, index) => {
             return (
               <li className="flex gap-1 items-center capitalize group" key={index}>
                 <ChevronsRight className="opacity-0 group-hover:opacity-100 " />
-                <Link href={page.url} className="text-3xl hover:" >
+                <Link href={page.url} className="text-3xl" >
+                  <span className="text-base">go to </span>
                   {page.title}
                 </Link>
               </li>
@@ -43,7 +45,9 @@ export default function Home() {
           }
 
         </ul>
-
+         <div className="mt-10">
+         <About />
+         </div>
       </div>
     </main>
   )
