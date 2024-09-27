80ed556cb970ea119f605f6ac1f31d08c315bf48
charles-mrt <charlesmartins.ads@gmail.com>
2024-09-27 16:59:06 -0300

    fix: add muted attribute to audio elements for autoplay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
 
     <main className="relative">
 
-      <audio src={"/assets/audio/city.mp3"} autoPlay loop />
-      <audio src={"/assets/audio/skate.mp3"} autoPlay loop />
+      <audio src={"/assets/audio/city.mp3"} autoPlay loop muted />
+      <audio src={"/assets/audio/skate.mp3"} autoPlay loop muted />
 
       <span className="text-6xl welcome-shadow" >bem vindo a </span>
       <h1 className="text-9xl  title-shadow" >My Code Draws </h1>
@@ -45,9 +45,9 @@ export default function Home() {
           }
 
         </ul>
-         <div className="mt-10">
-         <About />
-         </div>
+        <div className="mt-10">
+          <About />
+        </div>
       </div>
     </main>
   )
